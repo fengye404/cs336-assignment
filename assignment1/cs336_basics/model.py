2e63a6a96465686c9598e343bdb83bb0825e8a85
fengye <fengye4302@gmail.com>
2026-08-24 02:17:34 +0800

    Add RoPE module scaffold and notes

--- assignment1/cs336_basics/model.py
+++ assignment1/cs336_basics/model.py
@@ -149,3 +149,68 @@ class SwiGLU(torch.nn.Module):
         b = self.w3(x)
         # 注意这里是是逐元素相乘
         return self.w2(a * b)
+    
+
+# 传统 Transformer 的绝对位置编码：
+#
+# token_ids = [101, 205, 307]            # token 在词表中的 ID
+#              │    │    │
+#              │    │    └── token_embedding[307] = e₂
+#              │    └─────── token_embedding[205] = e₁
+#              └──────────── token_embedding[101] = e₀
+#
+# token 向量： [e₀,      e₁,      e₂]
+# 位置下标：    [ 0,       1,       2]
+#                │        │        │
+#                │        │        └── position_embedding[2] = p₂
+#                │        └─────────── position_embedding[1] = p₁
+#                └──────────────────── position_embedding[0] = p₀
+#
+# 位置向量： [p₀,      p₁,      p₂]
+#                │        │        │
+#                └────────逐元素相加────────┘
+#                             ↓
+# Transformer 输入：
+# x = [e₀ + p₀,  e₁ + p₁,  e₂ + p₂]
+#
+# ==================== 分界线 ==========================
+#
+# 而 RoPE 作用在在 Attention 的 q、k 向量上
+# 它会对每个 token 的 q、k 向量进行旋转，通过旋转来把位置信息编码到 q、k 中
+# 例如对于某个 token X[n]，RoPE 会对它的 q、k 向量进行如下操作：
+# 1、将 q、k 向量分为两两一组（q、k 向量的 shape 等于每个 token 的维度，现在的 transformer 里面 token 维度都是偶数）
+#    例如 q 为 [q0, q1, q2, q3, q4, q5]，
+#    会被分为 [(q0,q1), (q2,q3), (q4,q5)]
+#
+# 2、每一对维度都看作一个二维向量，并分别旋转
+#    每一对的旋转角度 = token 的位置 n × 该维度对自己的旋转频率
+#
+#    例如：
+#    (q0,q1) 按 n × f1 旋转
+#    (q2,q3) 按 n × f2 旋转
+#    (q4,q5) 按 n × f3 旋转
+#    f_k = 1 / theta^((2k - 2) / d_k)
+#
+#    不同维度对的频率 f1、f2、f3 不同，
+#    因此同一个 q/k 向量中的不同维度对，旋转角度通常也不同
+#
+# 3、q、k 的对应维度对，会按相同的位置 n 和相同频率旋转
+#    得到旋转后的 q_rotated、k_rotated；它们的 shape 不变
+#
+#    q: [q0, q1, q2, q3, q4, q5]
+#          ↓    ↓    ↓
+#    q_rotated: [q0', q1', q2', q3', q4', q5']
+#
+# 4、之后 Attention 使用旋转后的 q、k 计算点积
+#    q_rotated @ k_rotated.T
+#
+#    因此不同 token 的 q、k 点积不仅和内容有关，
+#    还会受到两者相对位置距离的影响
+class RotaryPositionalEmbedding(torch.nn.Module):
+    def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
+        # theta：RoPE 的频率基数
+        
+        pass
+    
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        pass
